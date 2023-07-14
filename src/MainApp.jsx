119a1c6a7a2791cafae6a290bdefd1b5c1aaa401
Abdul Hadi <hadi2408466@gmail.com>
2023-07-14 23:10:47 +0500

    navbar added

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import wallpaper from "./assets/wallpaper.jpg";
 import MainPage from "./MainPage";
 import TvShowPage from "./TvShowPage";
 export default function MainApp() {
   return (
-    <div className="overflow-hidden">
-      <MainPage />
+    <div className="w-screen relative ">
+      <div className="w-screen absolute flex p-6 justify-evenly">
+        <text className="text-black text-2xl px-8">Website Name</text>
+        <div className="m-auto">
+          <text className="text-black text-2xl px-8">Home</text>
+          <text className="text-black text-2xl px-8">Discover</text>
+          <text className="text-black text-2xl px-8">Movies</text>
+          <text className="text-black text-2xl px-8">TV Shows</text>
+        </div>
+      </div>
+      <div className="bg-cover opactiy-50">
+        <img src={wallpaper} />
+      </div>
     </div>
   );
 }
