15653bea40a560e8ae04804ee8a116a89f0c2af6
Abdul Hadi <hadi2408466@gmail.com>
2023-07-15 12:19:03 +0500

    set iamge

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -4,19 +4,30 @@ import MainPage from "./MainPage";
 import TvShowPage from "./TvShowPage";
 export default function MainApp() {
   return (
-    <div className="w-screen relative ">
-      <div className="w-screen absolute flex p-6 justify-evenly">
-        <text className="text-black text-2xl px-8">Website Name</text>
-        <div className="m-auto">
-          <text className="text-black text-2xl px-8">Home</text>
-          <text className="text-black text-2xl px-8">Discover</text>
-          <text className="text-black text-2xl px-8">Movies</text>
-          <text className="text-black text-2xl px-8">TV Shows</text>
+    <div className="max-w-full opacity-100 ">
+      <div className="w-full absolute flex p-6">
+        <text className="text-black text-2xl px-8 font-semibold">
+          Website Name
+        </text>
+        <div className="m-auto flex flex-1 max-w-full justify-end">
+          <div className="flex justify-between">
+            <text className="text-black text-2xl px-8 font-semibold">Home</text>
+            <text className="text-black text-2xl px-8 font-semibold">
+              Discover
+            </text>
+            <text className="text-black text-2xl px-8 font-semibold">
+              Trending
+            </text>
+            <text className="text-black text-2xl px-8 font-semibold">
+              Movies
+            </text>
+            <text className="text-black text-2xl px-8 font-semibold">
+              TV Shows
+            </text>
+          </div>
         </div>
       </div>
-      <div className="bg-cover opactiy-50">
-        <img src={wallpaper} />
-      </div>
+      <img src={wallpaper} />
     </div>
   );
 }
